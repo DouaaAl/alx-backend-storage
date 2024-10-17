f7096b51226f8864115356393db645df26ba72c7
root <elmahraouidouaa123@gmail.com>
2024-10-17 12:00:01 +0000

    complete 0 task

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,9 +1,6 @@
--- Choosing the database to use
-USE holberton;
-
 -- Creating table with contraints
 CREATE TABLE IF NOT EXISTS users (
-  id AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
   email VARCHAR(255) NOT NULL UNIQUE,
   name VARCHAR(255)
 );
